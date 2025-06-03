95bb90093bfdd2f11b76b87c87501b2c86e383d5
Beetle <drquinsey@gmail.com>
2025-06-03 06:59:18 -0400

    added u16s and u32s functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
-/// Produces a range as an array
-///
-/// # Examples
-///
-/// ```
-/// # use array_ranges::range;
-/// assert_eq!(range!(1u8,6u8), [1,2,3,4,5]);
-/// ```
+/**
+Evaluates to all the elements in `$start..$stop` as an array.
+
+## Examples
+
+```
+# use array_ranges::range;
+assert_eq!(range!(1u8,5u8), [1,2,3,4]);
+```
+*/
 #[macro_export]
 macro_rules! range {
     ($start: expr, $stop: expr) => {{
@@ -21,14 +23,16 @@ macro_rules! range {
     }};
 }
 
-/// Produces a range as an array
-///
-/// # Examples
-///
-/// ```
-/// # use array_ranges::range_inclusive;
-/// assert_eq!(range_inclusive!(1u8,5u8), [1,2,3,4,5]);
-/// ```
+/**
+Evaluates to all the elements in `$start..=$stop` as an array.
+
+## Examples
+
+```
+# use array_ranges::range_inclusive;
+assert_eq!(range_inclusive!(1u8,5u8), [1,2,3,4,5]);
+```
+*/
 #[macro_export]
 macro_rules! range_inclusive {
     ($start: expr, $stop: expr) => {{
@@ -43,3 +47,26 @@ macro_rules! range_inclusive {
         arr
     }};
 }
+
+#[must_use]
+pub const fn u8s() -> [u8; u8::MAX as usize + 1] {
+    type T = u8;
+    range_inclusive!(T::MIN, T::MAX)
+}
+
+#[must_use]
+pub const fn u16s() -> [u16; u16::MAX as usize + 1] {
+    type T = u16;
+    range_inclusive!(T::MIN, T::MAX)
+}
+
+#[must_use]
+pub const fn u32s() -> [u32; u32::MAX as usize + 1] {
+    type T = u32;
+    range_inclusive!(T::MIN, T::MAX)
+}
+
+// pub const fn u64s() -> [u64; u64::MAX as usize + 1] {
+//     type T = u64;
+//     range_inclusive!(T::MIN, T::MAX)
+// }
